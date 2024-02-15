6df464a18c97ceb413c4541adeabdf2f72cdd930
Melissa Mojica <melissa_mojica23@hotmail.com>
2024-02-14 20:00:04 -0500

    Add files via upload

---
+++ helloWorld.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start(){
+        print("Hello, world.");
+
+    }
+    /*
+     yo
+     yea
+     go
+     */
+     
+    // Update is called once per frame
+    void Update(){
+        
+    }
+}
